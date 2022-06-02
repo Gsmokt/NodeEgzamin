e9ea57b5fd688eb89f79ff828880e0333bcaf090
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-06-02 12:09:10 +0200

    new commit

--- ap.js
+++ ap.js
@@ -85,7 +85,7 @@ const checkUserMiddleware = async (req, res, next) => {
       await write(userPath, JSON.stringify(fl));
       next();
     } else {
-      res.status(401).send("No chance");
+      res.status(401).send("Bad token");
     }
   } else {
     if (token !== PASSWORD) res.status(401).send("Bad token");
@@ -98,6 +98,7 @@ const checkAuthorizationMiddleware = async (req, res, next) => {
   req.post = post;
   const token = req.headers["authorization"];
   if (!token) next();
+  if (token === "") res.status(400).send("Bad request");
   const [email, password] = token.split("&");
   try {
     const file = await read(userPath);
@@ -123,7 +124,7 @@ app.post("/add", checkAuthorizationMiddleware, (req, res) => {
   req.post
     .save()
     .then((_) => {
-      res.status(200).send("Done!");
+      res.status(201).send("Done!");
     })
     .catch((err) => {
       console.log(err.message);
@@ -131,7 +132,7 @@ app.post("/add", checkAuthorizationMiddleware, (req, res) => {
     });
 });
 
-app.get("/posts", (req, res) => {
+app.get("/post/all", (req, res) => {
   Post.find()
     .then((result) => {
       res.status(200).send(result);
@@ -148,9 +149,9 @@ app.get("/post/category/:category", (req, res) => {
   Post.find({ category: category }, (err, result) => {
     if (err) {
       console.log(err.message);
-      res.send("Not found");
+      res.status(404).send("Not found");
     } else if (result.length === 0) {
-      res.send("Not found");
+      res.status(404).send("Not found");
     } else {
       res.send(result);
     }
@@ -163,9 +164,9 @@ app.get("/post/author/:author", (req, res) => {
   Post.find({ author: author }, (err, result) => {
     if (err) {
       console.log(err.message);
-      res.send("Not found");
+      res.status(404).send("Not found");
     } else if (result.length === 0) {
-      res.send("Not found");
+      res.status(404).send("Not found");
     } else {
       res.send(result);
     }
@@ -178,9 +179,9 @@ app.get("/post/price/:min/:max", (req, res) => {
   Post.find({}, (err, result) => {
     if (err) {
       console.log(err.message);
-      res.send("Not found");
+      res.status(404).send("Not found");
     } else if (result.length === 0) {
-      res.send("Not found");
+      res.status(404).send("Not found");
     } else {
       const posts = result.filter(
         (post) => post.price >= min && post.price <= max
@@ -190,15 +191,35 @@ app.get("/post/price/:min/:max", (req, res) => {
   });
 });
 
+app.get("/post/date/:min/:max", (req, res) => {
+  const min = new Date(req.params.min);
+  const max = new Date(req.params.max);
+  Post.find({}, (err, result) => {
+    if (err) {
+      console.log(err.message);
+      res.status(404).send("Not found");
+    } else if (result.length === 0) {
+      console.log(result);
+      res.status(404).send("Not found");
+    } else {
+      const posts = result.filter((post) => {
+        const date = new Date(post.createdAt).toLocaleDateString();
+        const time = new Date(date).getTime();
+        if (time >= min && time <= max) return post;
+      });
+      res.send(posts);
+    }
+  });
+});
+
 app.get("/post/title/:title", (req, res) => {
   const title = req.params.title;
-
   Post.find({ title: title }, (err, result) => {
     if (err) {
       console.log(err.message);
-      res.send("Not found");
+      res.status(404).send("Not found");
     } else if (result.length === 0) {
-      res.send("Not found");
+      res.status(404).send("Not found");
     } else {
       res.send(result);
     }
@@ -229,47 +250,18 @@ app.delete("/post/:id", checkUserMiddleware, (req, res) => {
     });
 });
 
+app.put("/post/:id", checkUserMiddleware, (req, res) => {
+  const id = req.params.id;
+  Post.findByIdAndUpdate(id, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Could not update");
+    } else {
+      res.status(200).send("Post updated");
+    }
+  });
+});
+
 app.use("*", express.static(path.join(__dirname, "images/image.jpg")));
 
-// const checkUserMiddleware = async (req, res, next) => {
-//   const id = req.params.id;
-//   const token = req.headers['authorization'];
-//   // if(!token || token !== password) {
-//   //   res.status(401);
-//   //   res.send("bad token");
-//   // }
-//   if(token === password) {
-//     try{
-//     const file = await read('users.json');
-//     let fl = JSON.parse(file);
-//     // const user = fl.findIndex(e => e.email === email && e.password === password);
-//     // const available = fl.filter((e, i) => e.posts[i] === id);
-//     // if(available) {
-//       res.status(401);
-//       res.send("You are not authorized to delete this post");
-//     // }
-//   }catch(err) {
-//     res.status(500).send('Something wrong')
-//   }
-//   };
-//   if(token.includes('&')) {
-//     try{
-//     const [email, password] = token.split('&');
-//     const user = fl.findIndex(e => e.email === email && e.password === password );
-//     if(!user && user != 0) {
-//       console.log(user)
-//       res.status(401);
-//       res.send("bad token");
-//     }
-//     if(!user.posts.includes(id)){
-//       res.status(401);
-//       res.send("You can delete or modify only posts added by urself");
-//     }
-//     // fl[user].posts = fl[user].posts.filter(e => e !== id);
-//     // await write('users.json', JSON.stringify(fl));
-//     next();
-//   }catch(err) {
-//     res.status(500).send('Something wrong')
-//   }
-//   }
-// }
+
